8e316efecc7d74f18edbb404c3c5a4e01babf29e
SP <spetrovi@localhost.localdomain>
2021-03-22 08:02:07 +0100

    Adding make_prediction func to predict all stocks in path
    Fixing save_for_plot
    
    Signed-off-by: SP <spetrovi@localhost.localdomain>

--- utilities.py
+++ utilities.py
@@ -29,12 +29,11 @@ def normalise(price, high, low):
 def denormalise(price, high=86.716, low=15.965):
     return price * (high - low) + low
     
-def save_for_plot(g_model, dataset, name):
+def save_for_plot(g_model, dataset, numsteps, name):
     lows_real = []
     lows_pred = []
-    for i in range(5, len(dataset)-1):
-    #for i in range(5, 20):
-        recent_data = np.array([dataset[i-5 : i]])
+    for i in range(numsteps, len(dataset)-1):
+        recent_data = np.array([dataset[i-numsteps : i]])
 
         mean = np.mean(recent_data)
         std = np.std(recent_data)
@@ -46,7 +45,8 @@ def save_for_plot(g_model, dataset, name):
         #denormalise
         prediction = mean + (prediction[0][-1] * std)
     
-        lows_real.append(dataset[i+1][2])
+        # dataset[0:5] > indexes 0..4, next value is at dataset[5]
+        lows_real.append(dataset[i][2])
         lows_pred.append(prediction[2])
 
     #write to file for later analysis
@@ -90,7 +90,35 @@ def load_all(name):
     d_model = tf.keras.models.load_model(name + 'd_model', compile=False)
     a_model = define_GAN(g_model, d_model)
     
-    g_model.compile(loss=my_mse, optimizer='adam',  metrics=['mse', 'mae', 'mape'])
+    g_model.compile(loss=my_gMSE, optimizer='adam',  metrics=[my_gMSE, my_gMAE_l, my_gMAE_h])
     d_model.compile(loss=my_dloss, optimizer='adam',  metrics=[my_dacc])
     
     return g_model, d_model, a_model
+    
+def make_predictions(g_model, path_to_stocks, ma_days, num_steps, batch_size, num_params):
+    stocks = glob.glob(path_to_stocks + '/*')
+    
+    for stock in stocks:
+        dataset = process_fxPro(stock, ma_days)
+        recent_data = np.array([dataset[len(dataset)-num_steps : len(dataset)]])
+        
+        generator = KerasBatchGenerator(dataset, num_steps, batch_size, num_params, skip_step=1)
+        _, _, g_MSE_lows, g_MSE_highs = g_model.evaluate(generator.generate_for_gmodel(), steps=100)
+        
+        mean = np.mean(recent_data)
+        std = np.std(recent_data)
+
+        #normalise
+        norm_r = (recent_data-mean)/std        
+        prediction = g_model.predict(norm_r)
+        print('Prediction for stock: ' + stock)
+        print('Previous days:')
+        print(recent_data)
+
+        print('Next day:')
+        print(mean + (prediction*std))
+        
+        print('Error for Lows: ' + str(g_MSE_lows*std))        
+        print('Error for Highs: ' + str(g_MSE_highs*std))        
+
+        print('\n')    
