5095e712917cbe90cb40708e0dca862484c78cb7
SP <spetrovi@localhost.localdomain>
2021-03-22 07:55:37 +0100

    Adding LSTM discriminator
    Signed-off-by: SP <spetrovi@localhost.localdomain>

--- models.py
+++ models.py
@@ -7,13 +7,13 @@ from functions import *
 def define_generator(num_steps=5, num_params=5):    
     A1 = Input(shape=(num_steps,num_params))
 
-    A2 = LSTM(num_params, return_sequences=True, input_shape=(num_steps,num_params))(A1)
+    A2 = LSTM(num_params, return_sequences=True, input_shape=(num_steps,num_params), dropout=0.2, recurrent_dropout=0.2)(A1)
     A21 = LeakyReLU()(A2)
     
-    A3 = LSTM(num_params, return_sequences=True, input_shape=(num_steps,num_params))(A21)
+    A3 = LSTM(num_params, return_sequences=True, input_shape=(num_steps,num_params), dropout=0.2, recurrent_dropout=0.2)(A21)
     A31 = LeakyReLU()(A3)    
     
-    A4 = Dropout(0.5)(A31)
+    A4 = Dropout(0.0)(A31)
 
     A5 = TimeDistributed(Dense(num_params))(A4)
     A51 = LeakyReLU()(A5)    
@@ -75,6 +75,42 @@ def define_discriminator(num_steps=6, num_params=5):
     merged.compile(loss=my_dloss, optimizer='adam',  metrics=[my_dacc])
     return merged
 
+def define_discriminator_LSTM(num_steps=5, num_params=5):
+    num_steps = num_steps+1
+    A1 = Input(shape=(num_steps,num_params))
+
+    A2 = LSTM(num_params, return_sequences=True, input_shape=(num_steps,num_params), dropout=0.2, recurrent_dropout=0.2)(A1)
+    A21 = LeakyReLU()(A2)
+    
+    A3 = LSTM(num_params, return_sequences=True, input_shape=(num_steps,num_params), dropout=0.2, recurrent_dropout=0.2)(A21)
+    A31 = LeakyReLU()(A3)    
+
+    A4 = Dropout(0.0)(A31)
+    
+    #Ok, here it gets messy, so I'll explain a bit
+    #We have some computation going on with, but our input shape is num_steps*num_params
+    #That means, that each node's output is array of num_steps elements
+    #But for prediction, we only want one!
+    #So we use Flatten. This takes the outputs and make a single straight array, that we densely connect to single neuron
+    A42 = Flatten(input_shape=(num_steps, 1))(A4)
+    A5 = Dense(1, activation='sigmoid')(A42)
+    #Ok, we have our predicted value in A5, however, we can't glue it to the original values,
+    #Tensorflow wants element of array to have the same dimmension
+    #Our input array, that we want to pass is num_steps arrays of num_params elements
+    #In order to glue our prediction, we need to make a fake num_params array
+    #To make it a num_params array, we can use RepeatVector to just repeat the predicted value
+    #Don't forget, later in loss function, we need to make the prediction back to single value    
+    A51 = RepeatVector(num_params)(A5)
+
+    #Reshaping, cause tf is stupid (or too smart actually)
+    A6 = tf.keras.layers.Reshape((1,num_params))(A51)
+
+    B1 = tf.keras.layers.Lambda(lambda x: x, name='B1')(A1)
+    C = tf.keras.layers.Concatenate(axis=1)([B1, A6])
+    
+    merged = Model(inputs=[A1], outputs=[C])
+    merged.compile(loss=my_dloss, optimizer='adam',  metrics=[my_dacc])
+    return merged    
 
     
 #define GAN by sequentialy combining generator and discriminator
